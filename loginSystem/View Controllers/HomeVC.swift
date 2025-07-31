2f137e0778a5063c6adcdce6edc5674c30730807
Daria Timoshevskaia <vileonika@gmail.com>
2025-07-31 20:39:04 +0700

    first commit; made some basics

---
+++ loginSystem/View Controllers/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginSystem
+//
+//  Created by Daria Timoshevskaia on 31.07.25.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
