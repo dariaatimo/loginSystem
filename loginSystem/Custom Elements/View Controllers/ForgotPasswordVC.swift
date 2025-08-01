0865772b5b0cd16ab05edd020ca35708f6ebaa74
Daria Timoshevskaia <vileonika@gmail.com>
2025-08-02 00:06:18 +0700

    made ForgotPasswordVC UI and HomeVC preps

--- loginSystem/Custom Elements/View Controllers/ForgotPasswordVC.swift
+++ loginSystem/Custom Elements/View Controllers/ForgotPasswordVC.swift
@@ -8,10 +8,63 @@
 import UIKit
 
 class ForgotPasswordVC: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private let headerView = AuthHeaderView(title: "Forgot password", subtitle: "Reset your password")
+    private let emailTextField = LSTextField(fieldType: .email)
+    private let resetPasswordButton = LSButton(title: "Reset password", hasBackground: true, fontSize: .big)
 
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemMint
+        setupUI()
+        
+        resetPasswordButton.addTarget(self, action: #selector(didTapForgotPassword), for: .touchUpInside)
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(headerView)
+        view.addSubview(emailTextField)
+        view.addSubview(resetPasswordButton)
+        
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        resetPasswordButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 230),
+            
+            emailTextField.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 12),
+            emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emailTextField.heightAnchor.constraint(equalToConstant: 55),
+            emailTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            resetPasswordButton.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 22),
+            resetPasswordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            resetPasswordButton.heightAnchor.constraint(equalToConstant: 55),
+            resetPasswordButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+        ])
+    }
+    
+    // MARK: - Selectors
+    
+    @objc private func didTapForgotPassword() {
+        guard let email = emailTextField.text, !email.isEmpty else { return }
+        
+        // TODO: Email validation
+        
+    }
 }
--- loginSystem/Custom Elements/View Controllers/HomeVC.swift
+++ loginSystem/Custom Elements/View Controllers/HomeVC.swift
@@ -9,9 +9,42 @@ import UIKit
 
 class HomeVC: UIViewController {
     
+    // MARK: - UI Components
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.text = "Loading..."
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemIndigo
+        setupUI()
+    }
+    
+    // MARK: - UI Setup
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(didTapLogout))
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    // MARK: - Selectors
+    
+    @objc private func didTapLogout() {
+        
     }
 
 }
